66754fdeac403d79cb7ac149271f651f71d682b8
SiroiluT <tulio-evangelista@outlook.com>
2025-09-16 01:53:26 -0300

    refactor: ajusta dependências e classpath para resolver erros

--- src/main/java/com/Java/Imobiliaria/clientes/repository/ClientesRepository.java
+++ src/main/java/com/Java/Imobiliaria/clientes/repository/ClientesRepository.java
@@ -2,13 +2,17 @@ package com.Java.Imobiliaria.clientes.repository;
 
 import com.Java.Imobiliaria.clientes.model.ClientesModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Repository
 public interface ClientesRepository extends JpaRepository<ClientesModel, Long> {
+
+    Optional<UserDetails> findByEmail(String userEmail);
 
 
 
--- src/main/java/com/Java/Imobiliaria/configuration/TokenService.java
+++ src/main/java/com/Java/Imobiliaria/configuration/TokenService.java
@@ -4,10 +4,13 @@ package com.Java.Imobiliaria.configuration;
 import com.Java.Imobiliaria.clientes.model.ClientesModel;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Optional;
 
 @Component
 public class TokenService {
@@ -26,6 +29,30 @@ public class TokenService {
                 .withIssuedAt(Instant.now())
                 .withIssuer("Imobiliaria API")
                 .sign(algorithm);
+    }
+    public Optional<JWTUserData> validarToken(String token) {
+
+        try {
+
+            Algorithm algorithm = Algorithm.HMAC256(palavraSecreta);
+
+            DecodedJWT verify = JWT.require(algorithm)
+                    .withIssuer("imobiliaria")
+                    .build()
+                    .verify(token);
+
+            return Optional.of(JWTUserData
+                    .builder()
+                    .id(verify.getClaim("id").asLong())
+                    .name(verify.getClaim("name").asString())
+                    .email(verify.getSubject())
+                    .build());
+
+
+        } catch (JWTVerificationException ex) {
+            return Optional.empty();
+        }
+
     }
 
 }
